396584aa04688354b9358a302f8a6a36ef226f2d
yebana <yebana@outlook.com>
2025-05-21 11:07:43 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,8 +112,7 @@ with col1:
     st.metric("Nota de Acceso (hasta 10)", f"{nota_acceso:.3f}")
 
 with col2:
-    #st.metric("Nota Final (hasta 14)", f"{nota_final:.3f}")
-    st.markdown(f"**:redNota Final (sobre 14) :** :red[{nota_final:.2f}]")
+    st.metric("Nota Final (hasta 14)", f"{nota_final:.3f}")
 
 # Mostrar notas específicas con las dos mejores en rojo
 st.markdown("#### 📝 Desglose de Notas Específicas")
